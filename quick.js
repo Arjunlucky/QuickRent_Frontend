26b42e10c11a87325cfe0b9df79a1ea2ee02a99b
Nandireddy Nagarjun Reddy <96239766+Arjunlucky@users.noreply.github.com>
2024-12-14 19:25:40 +0530

    Add files via upload

---
+++ quick.js
@@ -0,0 +1,7 @@
+// Add animations (optional)
+document.addEventListener('scroll', () => {
+    const features = document.querySelector('.features');
+    const gadgets = document.querySelector('.gadgets');
+    features.style.opacity = 1;
+    gadgets.style.opacity = 1;
+  });
